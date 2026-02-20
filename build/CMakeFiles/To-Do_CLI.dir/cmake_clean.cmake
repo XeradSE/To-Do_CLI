e2056357255e8cfde2dc0d0ccfda1ebe8b0f0509
Kylian Betuel <betuelkylian17@gmail.com>
2026-02-20 23:58:18 +0100

    first commit

---
+++ build/CMakeFiles/To-Do_CLI.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/To-Do_CLI.dir/link.d"
+  "CMakeFiles/To-Do_CLI.dir/main.cpp.o"
+  "CMakeFiles/To-Do_CLI.dir/main.cpp.o.d"
+  "To-Do_CLI"
+  "To-Do_CLI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/To-Do_CLI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/To-Do_CLI.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for To-Do_CLI.
+# This may be replaced when dependencies are built.
